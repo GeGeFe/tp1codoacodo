468c13489ae3dc4f7635fdcbbba8eced65f91d7a
LuciaFederico <113947996+LuciaFederico@users.noreply.github.com>
2022-10-24 23:29:03 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,47 +1,37 @@
-var nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var mail = document.getElementById("mail");
-var telefono = document.getElementById("telefono");
-var texto = document.getElementById("texto");
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const mail = document.getElementById('mail');
+const telefono = document.getElementById('telefono');
+const texto = document.getElementById('texto');
+const form = document.getElementById('consulta');
+const errorElement = document.getElementById('error');
 
-function EnviarFormulario(){
-    console.log("consulta enviada");
-    var mensajeError = [];
-    var error = [];
-    var enviar = true;
-    if(nombre.value === null || nombre.value === ""){
-        mensajeError.push("ingresa un nombre");
-        enviar = false;
-        console.log(mensajeError);
-    }
-    if(apellido.value === null || apellido.value === ""){
-        mensajeError.push("ingresa un apellido");
-        enviar = false;
-        console.log(mensajeError);
-        print(mensajeError);
-        
-    }
-    if(mail.value === null || mail.value === ""){
-        mensajeError.push("ingresa un mail");
-        enviar = false;
-        console.log(mensajeError);
-        print(mensajeError);
-    }
-    if(telefono.value === null || telefono.value === ""){
-        mensajeError.push("ingresa un telefono");
-        enviar = false;
-        console.log(mensajeError);
-        print(mensajeError);
-    }
-    if(texto.value === null || texto.value === ""){
-        mensajeError.push("ingresa tu consulta");
-        enviar = false;
-        console.log(mensajeError);
-        print(mensajeError);
-    }
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (nombre.value === '' || nombre.value == null) {
+    messages.push('Escriba su nombre')
+  }
+  if (apellido.value === '' || apellido.value == null) {
+    messages.push('Escriba su apellido')
+  }  
+  if (mail.value === '' || mail.value == null) {
+    messages.push('Ingrese un mail válido')
+  }  
+  if (telefono.value.lenght <10) {
+    messages.push('Teléfono inválido')
+  }
+  if (telefono.value.length >10) {
+    messages.push('Teléfono inválido')
+  }  
+  if (texto.value === '' || texto.value == null) {
+    messages.push('Ingrese su consulta')
+  }  
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
     
-       error.innerHTML = mensajeError.join(",");
     
-    return enviar;
-   
-}
+    
+    
